2b45a692b10e57084428776f24e9354cce8af101
LansanaF <lansanafofana004@gmail.com>
2023-01-02 18:22:13 +0100

    Restful project

---
+++ Back-end/app/models/car.model.js
@@ -0,0 +1,16 @@
+const  mongoose  = require('mongoose');
+
+const carSchema = mongoose.Schema({
+    brand: {type: String},
+    horse_power: {type: Number},
+    num_doors: {type: Number},
+    model: {type: String}
+}, {
+    writeConcern: {
+      w: 'majority',
+      j: true,
+      wtimeout: 1000
+    }
+  });
+
+module.exports = mongoose.model('Car', carSchema);
